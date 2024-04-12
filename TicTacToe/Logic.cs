9f0e500a6ce68ed87f4ddea4851ef66e6a5c2056
Alexandru Panaitoiu <alexpanaitoiu@gmail.com>
2024-04-12 16:38:57 +0200

    Split between UI LOGIC DATA

---
+++ TicTacToe/Logic.cs
@@ -0,0 +1,65 @@
+﻿namespace TicTacToe
+{
+    public class Logic
+    {
+        public static bool IsValidMove(char[,] grid, int row, int column)
+        {
+            // Checking for invalid move
+            return row >= 0 && row < Program.GRID_SIZE && column >= 0 && column < Program.GRID_SIZE && grid[row, column] == ' ';
+        }
+
+        public static bool IsGameOver(char[,] grid)
+        {
+            // Checking if the game is over
+            return CheckWinner(grid) != ' ' || IsBoardFull(grid);
+        }
+
+        public static char CheckWinner(char[,] grid)
+        {
+            // Checking if there is a winner
+            for (int i = 0; i < Program.GRID_SIZE; i++)
+            {
+                if (grid[i, 0] != ' ' && grid[i, 0] == grid[i, 1] && grid[i, 1] == grid[i, 2])
+                    return grid[i, 0];
+                if (grid[0, i] != ' ' && grid[0, i] == grid[1, i] && grid[1, i] == grid[2, i])
+                    return grid[0, i];
+            }
+
+            if (grid[0, 0] != ' ' && grid[0, 0] == grid[1, 1] && grid[1, 1] == grid[2, 2])
+                return grid[0, 0];
+            if (grid[0, 2] != ' ' && grid[0, 2] == grid[1, 1] && grid[1, 1] == grid[2, 0])
+                return grid[0, 2];
+
+            return ' '; // No winner
+        }
+
+        public static bool IsBoardFull(char[,] grid)
+        {
+            // Checking if the grid is full
+            for (int i = 0; i < Program.GRID_SIZE; i++)
+            {
+                for (int j = 0; j < Program.GRID_SIZE; j++)
+                {
+                    if (grid[i, j] == ' ')
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public static void InitializeGrid(char[,] grid)
+        {
+            // Initialize the grid
+            for (int i = 0; i < Program.GRID_SIZE; i++)
+            {
+                for (int j = 0; j < Program.GRID_SIZE; j++)
+                {
+                    grid[i, j] = ' ';
+                }
+            }
+        }
+    }
+}
+
+
+
---
+++ TicTacToe/UI.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    public class UI
+    {
+        public static void DisplayGrid(char[,] grid)
+        {
+            // Displaying the grid
+            Console.Write("  ");
+            for (int j = 0; j < Program.GRID_SIZE; j++)
+            {
+                Console.Write((j + 1) + " ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < Program.GRID_SIZE; i++)
+            {
+                Console.Write((i + 1) + " ");
+                for (int j = 0; j < Program.GRID_SIZE; j++)
+                {
+                    if (grid[i, j] != ' ')
+                    {
+                        Console.Write(grid[i, j]);
+                    }
+                    else
+                    {
+                        Console.Write("_");
+                    }
+
+                    if (j < Program.GRID_SIZE - 1)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void UserMove(char[,] grid)
+        {
+            // Allow the user to make a move
+            bool validMove = false;
+            while (!validMove)
+            {
+                Console.Write("Choose a row between (1, 2 or 3): ");
+                int row = int.Parse(Console.ReadLine()) - 1;
+                Console.Write("Choose a column between (1, 2 or 3): ");
+                int column = int.Parse(Console.ReadLine()) - 1;
+
+                if (Logic.IsValidMove(grid, row, column))
+                {
+                    grid[row, column] = 'X';
+                    validMove = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid spot. Please try again.");
+                }
+            }
+        }
+
+        public static void AIMove(char[,] grid)
+        {
+            // AI move
+            Random rand = new Random();
+            int row, column;
+            do
+            {
+                row = rand.Next(0, Program.GRID_SIZE);
+                column = rand.Next(0, Program.GRID_SIZE);
+            } while (!Logic.IsValidMove(grid, row, column));
+
+            grid[row, column] = 'O';
+        }
+
+        public static void DisplayGameOverMessage(char winner)
+        {
+            // Displaying the end of the game
+            if (winner != ' ')
+            {
+                Console.WriteLine("Well done, " + winner + " is the winner!");
+            }
+            else
+            {
+                Console.WriteLine("Draw!");
+            }
+        }
+
+        public static void DisplayInvalidMoveMessage()
+        {
+            // Displaying the invalid input message
+            Console.WriteLine("Invalid spot. Please try again.");
+        }
+    }
+}
+
